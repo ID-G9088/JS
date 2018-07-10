d59018d35cf7675c85840831f0c38727439ad2c0
Igor G <igorogi.g@gmail.com>
2018-07-10 17:53:46 +0300

    2

--- alert.js
+++ alert.js
@@ -1,7 +1,52 @@
-var name = "VASILIY";
+/*var name = "VASILIY";
 var admin;
 admin=name;
 alert(admin);
 var planetName="ZEMLYA";
 var userName="petya";
-alert(a*2);
+var lastName = prompt ("What is you last name?" , "");
+alert ("good day, Mr. " + lastName);
+var result=confirm("are you boy?");
+alert(result);
+var companyname = prompt ("Каково «официальное» название JavaScript?","");
+if (companyname=="Ecma") {
+  alert("Verno");
+}
+else {
+  alert("ЛОХ");
+}
+var numbers = prompt ("ВВЕДИ ЧИСЛО ДУРАК","");
+if (numbers>0) {
+  alert(1);
+}
+else if (numbers<0){
+  alert(-1);
+}
+else {
+  alert(0);
+}
+var login = prompt ("Кто пришел","");
+if (login=="admin") {
+    var pass = prompt ("Пароль","");
+    if (pass=="password") {
+    alert("HI");
+    }
+    else if (pass==null) {
+      alert('pass canceled');
+    }
+    else {alert("INCORRECT PASSWOD");
+  }
+
+}
+else if (login==null) {
+alert('Login canceled')
+}
+else {
+  alert("i dont know you");
+}
+result = (a + b < 4) ? 'Мало' : 'Много';*/
+ 
+var message = (login=="VASIA") ? "PRIVET":
+(login=="DIREKTOR") ? "DOBRII BE4eR":
+(login=="") ? "net logina": "";
+alert(message);
